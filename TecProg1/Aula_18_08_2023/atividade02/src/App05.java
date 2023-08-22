1955eade431bc9bba6b67ea211ca1ef8114a98b2
GabrielDourado <82122849+gabrieldourado77@users.noreply.github.com>
2023-08-21 21:20:47 -0300

    Add files via upload

---
+++ TecProg1/Aula_18_08_2023/atividade02/src/App05.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class App05 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = 0;
+
+        System.out.println("------------------------------------");
+        System.out.print("Digite um número inteiro: ");
+        num = sc.nextInt();
+        System.out.println("------------------------------------");
+
+        if(num % 2 == 0){
+            System.out.println("O número "+num+" é par");
+        }
+        else{
+            System.out.println("O número "+num+" é ímpar");
+        }
+
+        System.out.println("------------------------------------");
+
+    }
+}

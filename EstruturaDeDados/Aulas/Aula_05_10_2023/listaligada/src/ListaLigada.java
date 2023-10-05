4e23d1b8de517b11e04294449246c55543acf761
Gabriel Dourado dos Santos <82122849+gabrieldourado77@users.noreply.github.com>
2023-10-05 09:18:36 -0300

    Add files via upload

---
+++ EstruturaDeDados/Aulas/Aula_05_10_2023/listaligada/src/ListaLigada.java
@@ -0,0 +1,54 @@
+public class ListaLigada {
+
+    private No inicio;
+    
+    public ListaLigada(){
+        inicio = null;
+    }
+
+    public void adicionaInicio(int e){
+        No novo = new No(e);
+        novo.prox = inicio;
+        inicio = novo;
+    }
+
+    public void adicionaFinal(int e){
+        if(inicio == null){
+            inicio = new No(e);
+        }
+        else{
+            No aux = inicio;
+
+            while(aux.prox != null){
+                aux = aux.prox;
+            }
+
+            No novo = new No(e);
+            aux.prox = novo;
+        }
+    }
+
+    public int removeInicio() throws Exception{
+        if(inicio == null){
+            throw new Exception("Erro! Lista Vazia."); 
+        }
+        else{
+            int r = inicio.dado;
+            inicio = inicio.prox;
+            return r;
+        }
+    }
+
+    public String toString(){
+        String r = "";
+        No aux = inicio;
+
+        while(aux != null){
+            r = aux.dado + "\t";
+            aux = aux.prox;
+        }
+
+        return r;
+    }
+
+}
